5904f51bdf179bf140b867bc4cc37e87d478afca
Николай <talam0nal@gmail.com>
2020-04-24 15:59:05 +0300

    Удалил ненужное

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,6 @@
 
 @section('content')
 <div class="container">
-    <!--
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Сообщение</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Вы успешно вошли в систему!
-                </div>
-            </div>
-        </div>
-    </div>
-    -->
 
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -36,7 +17,6 @@
             @endif
         </div>
     </div>
-
 
     @if (!$isManager)
         <!-- Форма отправки заявки -->
