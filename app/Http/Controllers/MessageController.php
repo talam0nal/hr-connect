8c9c3d226f836eab9e2b50bf8c52a3d7e3bccc9e
Николай <talam0nal@gmail.com>
2020-04-24 16:44:49 +0300

    Сохранение сообщений в контроллере

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Message;
+
+class MessageController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $message = Message::create([
+            'theme'     => $request->theme,
+            'message'   => nl2br($request->message),
+            'user_id'   => \Auth::id(),
+            'ticket_id' => $request->ticket_id,
+        ]);
+        return redirect()->route('home')->with('success', 'Ваше сообщение успешно отрпавлено');
+    }
+
+}
