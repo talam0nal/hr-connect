a6b6c461547c0e62111d1a0040c403210f35400a
Николай <talam0nal@gmail.com>
2020-04-24 16:45:52 +0300

    [миграция] Таблица с сообщениями

---
+++ database/migrations/2020_04_24_132247_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('theme');
+            $table->text('message');
+            $table->text('file')->nullable();
+            $table->integer('user_id');
+            $table->integer('ticket_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
