f5fe49d3c58eaf69cbdf0ba5e3cc5942f5ed8bfa
Николай <talam0nal@gmail.com>
2020-04-23 17:01:32 -0700

    [миграция] Таблица с заявками

---
+++ database/migrations/2020_04_24_015346_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('theme');
+            $table->longText('message');
+            $table->text('file')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('is_closed')->default(0);
+            $table->tinyInteger('is_viewed')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
