fa4c7d65bb3d966eb9c9fe805a35dd33f16fe345
Николай <talam0nal@gmail.com>
2020-04-24 15:53:35 +0300

    Вывод списка заявок пользователя

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Ticket;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $isManager = \Auth::user()->is_manager;
+        $tickets = Ticket::where('user_id', \Auth::id())->get();
+        return view('home', compact('isManager', 'tickets'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
     </div>
     -->
 
-    <div class="row justify-content-center mt-5">
+    <div class="row justify-content-center">
         <div class="col-md-8">
             @if (\Session::has('success'))
                 <div class="bd-example">
@@ -38,34 +38,72 @@
     </div>
 
 
-    <!-- Форма отправки заявки -->
-    <div class="row justify-content-center">
-        <div class="col-md-8 mt-5">
-            <h2>Добавить новую заявку</h2>
-            <form method="POST" enctype="multipart/form-data" action="{{ route('tickets.store') }}">
-                @csrf
-                <div class="form-group">
-                    <label for="theme">Тема</label>
-                    <input name="theme" class="form-control" id="theme" placeholder="Введите тему..." required>
-                </div>
+    @if (!$isManager)
+        <!-- Форма отправки заявки -->
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h2>Добавить новую заявку</h2>
+                <form method="POST" enctype="multipart/form-data" action="{{ route('tickets.store') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="theme">Тема</label>
+                        <input name="theme" class="form-control" id="theme" placeholder="Введите тему..." required>
+                    </div>
 
-                <div class="form-group">
-                    <label for="message">Сообщение</label>
-                    <textarea name="message" class="form-control" id="message" rows="3" placeholder="Введите сообщение..." required></textarea>
-                </div>
+                    <div class="form-group">
+                        <label for="message">Сообщение</label>
+                        <textarea name="message" class="form-control" id="message" rows="3" placeholder="Введите сообщение..." required></textarea>
+                    </div>
 
-                <div class="form-group">
-                    <label for="file">Прикрепите файл</label>
-                    <input type="file" name="attachment" class="form-control-file" id="file">
-                </div>
+                    <div class="form-group">
+                        <label for="file">Прикрепите файл</label>
+                        <input type="file" name="attachment" class="form-control-file" id="file">
+                    </div>
 
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Отправить</button>
-                </div>
-            </form>
-
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Отправить</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
+
+        <div class="row justify-content-center mt-3">
+            <div class="col-md-8">
+                <h2>Ваши заявки</h2>
+
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Дата</th>
+                      <th scope="col">Тема</th>
+                      <th scope="col">Статус</th>
+                      <th scope="col">Сообщений</th>
+                      <th scope="col">Управление</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach ($tickets as $ticket)
+                        <tr>
+                          <th scope="row">{{ $ticket->created_at }}</th>
+                          <td><a href="#">{{ $ticket->theme }}</a></td>
+                          <td>@if ($ticket->status)Принята в обработку @else Ждёт обработки @endif</td>
+                          <td>@mdo</td>
+                          <td><button type="button" class="btn btn-info">Закрыть</button></td>
+                        </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+
+            </div>
+        </div>
+
+        @else
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h2>Заявки</h2>
+            </div>
+        </div>
+    @endif
 
 
 </div>
