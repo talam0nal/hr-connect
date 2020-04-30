0849f893d34086753ee9332265ccdad920d5f853
Николай <talam0nal@gmail.com>
2020-04-30 10:20:02 +0300

    Revert "Удалил ненужный метод"
    This reverts commit 92cd595da89da0b0f9fd97fc9aabafa35b438ee9.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,4 +17,13 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+    }
 }
