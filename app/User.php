49a46057e24ac11ec2c10941858d63629d430421
Николай <talam0nal@gmail.com>
2020-04-30 13:24:26 +0300

    Скоуп для менеджера в модели пользователя

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeIsManager($query)
+    {
+        return $query->where('is_manager', 1);
+    }
+
 }
