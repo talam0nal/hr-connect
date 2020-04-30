264aa89efc92340ed0d566c1a2f4d3f7c5903b4e
Николай <talam0nal@gmail.com>
2020-04-30 11:53:47 +0300

    m

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -14,6 +14,7 @@ class TicketController extends Controller
     */
     public function index()
     {
+        
         if (!\Auth::check()) {
             return redirect()->route('login');
         }
