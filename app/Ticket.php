2ffe654738844280bf60b474905d22ca671ccacc
Николай <talam0nal@gmail.com>
2020-04-30 12:16:21 +0300

    Дополнительные скоупы для заявок

--- app/Ticket.php
+++ app/Ticket.php
@@ -29,6 +29,27 @@ class Ticket extends Model
     public function scopeByCurrentUser($query)
     {
         return $query->where('user_id', \Auth::id());
-    }    
-    
+    }
+
+    public function scopeOnlyViewed($query)
+    {
+        return $query->where('is_viewed', 1);
+    }
+
+    public function scopeOnlyUnViewed($query)
+    {
+        return $query->where('is_viewed', 0);
+    }
+
+    public function scopeOnlyClosed($query)
+    {
+        return $query->where('is_closed', 1);
+    }
+
+
+    public function scopeOnlyUnClosed($query)
+    {
+        return $query->where('is_closed', 0);
+    }
+
 }
