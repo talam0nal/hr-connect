5767d45270b8411cb12545e446b86dd7f09a44bd
Николай <talam0nal@gmail.com>
2020-04-30 08:20:20 +0300

    Удалил ненужные методы

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -25,16 +25,6 @@ class TicketController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -60,17 +50,6 @@ class TicketController extends Controller
     {
         $ticket = Ticket::findOrFail($id);
         return view('show', compact('ticket'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
