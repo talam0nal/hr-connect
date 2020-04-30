92cd595da89da0b0f9fd97fc9aabafa35b438ee9
Николай <talam0nal@gmail.com>
2020-04-30 09:34:54 +0300

    Удалил ненужный метод

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,13 +17,4 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-
-    }
 }
