e2f2c20757a4cd97b4332c5e76720f09cee8feeb
Николай <talam0nal@gmail.com>
2020-04-30 12:58:16 +0300

    Локализация

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Введён неверный логин или пароль',
+    'throttle' => 'Слишком большое количество попыток входа. Попробуйте заново через :seconds seconds.',
 
 ];
